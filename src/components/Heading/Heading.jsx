cc3d4bb2b4c4570b4dc09673fe859b9d63740e2e
abx15 <arunkumarbind150720@gmail.com>
2025-10-09 16:36:46 +0530

    Heading color

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = (props) => {
     return (
         <div className='w-fit mx-auto'>
-            <h2 className='text-5xl font-bold text-[2.5rem]'><span className='text-orange-500'>{props.highlight}</span> {props.heading}</h2>
+            <h2 className=' text-zinc-800 text-5xl font-bold text-[2.5rem]'><span className='text-orange-500'>{props.highlight}</span> {props.heading}</h2>
             <div className='w-35 h-1 bg-orange-300 md:mt-6 ml-auto '></div>
         </div>
     )
