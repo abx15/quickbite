c46d5ab3f22a6e5ab9dd06a6a77662c46644a54c
abx15 <arunkumarbind150720@gmail.com>
2025-10-11 13:25:23 +0530

    testimonials section

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Testimonials = () => {
+  return (
+    <setion>
+        <div className='max-w-[1400px] px-10 mx-auto py-20'>
+<Heading highlight='Customers' heading='Saying' />
+        </div>
+      
+    </setion>
+  )
+}
+
+export default Testimonials
