e383f5a44d475a389b8ae07fe80c43393cbd500a
abx15 <arunkumarbind150720@gmail.com>
2025-10-11 14:20:31 +0530

    some chenges

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 const Testimonials = () => {
-  return (
-    <setion>
-        <div className='max-w-[1400px] px-10 mx-auto py-20'>
-<Heading highlight='Customers' heading='Saying' />
-        </div>
-      
-    </setion>
-  )
+    return (
+        <setion>
+            <div className='max-w-[1400px] px-10 mx-auto py-20'>
+                <Heading highlight='Customers' heading='Saying' />
+                <div className='py-5 flex justify-end gap-x-3'>
+                    <button className='text-2xl rounded-lg q-11 h-11 bg-zinc-100 hover:bg-orange-500 text-zinc-800 cursor-pointer flex justify-center items-center'>
+                        <IoIosArrowBack />
+
+                    </button>
+                    <button className='text-2xl rounded-lg q-11 h-11 bg-zinc-100 hover:bg-orange-500 text-zinc-800 cursor-pointer flex justify-center items-center'>
+                        <IoIosArrowForward />
+
+                    </button>
+                </div>
+            </div>
+
+        </setion>
+    )
 }
 
 export default Testimonials
